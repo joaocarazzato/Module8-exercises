79aa66f1946740d509f015766d04e2ffa9b7c5db
joaocarazzato <joao.carazzato@sou.inteli.edu.br>
2023-11-09 14:36:21 -0300

    removed unused files

--- entregavel01/src/ros2_pkg_entregavel/build/ros2_pkg_entregavel/build/lib/ros2_pkg_entregavel/teste.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Ola, mundo!")
-
-if __name__ == "__main__":
-    main()
--- entregavel01/src/ros2_pkg_entregavel/build/ros2_pkg_entregavel/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/joao/Documents/GitHub/Module8-exercises/entregavel01/src/ros2_pkg_entregavel/install/ros2_pkg_entregavel'
